2c425e0b26d2b95f286fcfabbfde858e041c05fc
Manish Yadav <manishyaduvanshi8294@gmail.com>
2025-06-18 19:01:33 +0530

    Add New project, random Number guess project

---
+++ Number Guessing Game/script.js
@@ -0,0 +1,39 @@
+let userInputEl = document.getElementById("userInput");
+let outputCheckBtnEl = document.getElementById("outputCheckBtn");
+let gameResultEl = document.getElementById("gameResult");
+
+
+let maxGuessedMsg = "Too High! Try Again";
+let minGuessedMsg = "Too Low! Try Again";
+let congratulationsMsg = "Congratulations! You got it right."
+let invalidInputMsg = "Please provide a valid input.";
+
+
+let randomNum = Math.ceil(Math.random() * 100)
+
+
+let checkUserGuessedNumber = () => {
+    let guessedNumber = parseInt(userInputEl.value);
+
+
+    if (guessedNumber > randomNum) {
+        gameResult.textContent = maxGuessedMsg;
+        gameResult.style.backgroundColor = "#1e217c";
+    }
+    else if (guessedNumber < randomNum) {
+        gameResult.textContent = minGuessedMsg;
+        gameResult.style.backgroundColor = "#1e217c";
+    }
+    else if (guessedNumber === randomNum) {
+        gameResult.textContent = congratulationsMsg;
+        gameResult.style.backgroundColor = "green";
+    }
+    else {
+        gameResult.textContent = invalidInputMsg;
+        gameResult.style.backgroundColor = "#1e217c";
+    }
+
+}
+
+
+outputCheckBtnEl.addEventListener("click", checkUserGuessedNumber); 
